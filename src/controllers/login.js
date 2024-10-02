af1ac74a37e4dc60fc1608e369d1f7b4abaace8d
meet26122020 <140876816+meet26122020@users.noreply.github.com>
2024-10-03 02:19:37 +0530

    Add files via upload

---
+++ src/controllers/login.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const createHttpError = require("http-errors");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        console.log(req.body);
+        const user = await User.findOne({ email }).exec();
+
+        if (user) {
+            const correctPass = bcrypt.compare(password, user.password);
+
+            if (correctPass) {
+                jwt.sign(
+                    { email: user.email, id: user._id },
+                    process.env.JWT_SECRET,
+                    {},
+                    (err, token) => {
+                        if (err) throw err;
+                        res.cookie("token", token).json(user);
+                    }
+                );
+            } else {
+                throw new Error("Invalid credentials");
+            }
+        } else {
+            throw new Error("User not found");
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = loginUser;
---
+++ src/controllers/uploadFromDevice.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const uploadPhotoFromDevice = async (req, res, next) => {
+    try {
+        console.log(req.files.length);
+        const uploadedFiles = [];
+        for (let i = 0; i < req.files.length; i++) {
+            const { path, originalname } = req.files[i];
+            const parts = originalname.split(".");
+            console.log(parts);
+            const extension = parts[parts.length - 1];
+            const newPath = path + "." + extension;
+            fs.renameSync(path, newPath);
+            uploadedFiles.push(
+                newPath.replace(
+                    "/Users/marioangelov/Desktop/Airbnb/server/assets/uploads/",
+                    ""
+                )
+            );
+        }
+        res.json(uploadedFiles);
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+};
+
+module.exports = uploadPhotoFromDevice;
